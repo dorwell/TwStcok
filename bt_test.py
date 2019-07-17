63e16435bb8c6089bf1dbb1a0dc81062c4d28de2
Dorwell <dorwell@gmail.com>
2019-07-17 16:02:47 +0800

    Add optimizer for firstStrategy: 1. minlevel 2. maxlevel

--- bt_test.py
+++ bt_test.py
@@ -11,8 +11,58 @@ import locale
 from locale import atof
 import time
 
-class firstStrategy(bt.Strategy):
+# Create a Stratey
+class MyStrategy(bt.Strategy):
+    params = (
+        ('ssa_window', 15),
+        ('maperiod', 15),
+    )
+ 
+    def log(self, txt, dt=None):
+        ''' Logging function fot this strategy'''
+        dt = dt or self.datas[0].datetime.date(0)
+        print('%s, %s' % (dt.isoformat(), txt))
+ 
+    def __init__(self):
+        # Keep a reference to the "close" line in the data[0] dataseries
+        self.dataclose = self.datas[0].close
+ 
+        # To keep track of pending orders and buy price/commission
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+ 
+        # Add a MovingAverageSimple indicator
+        # self.ssa = ssa_index_ind(
+        #     self.datas[0], ssa_window=self.params.ssa_window)
+        self.sma = bt.indicators.SimpleMovingAverage(
+            self.datas[0], period=self.params.maperiod)
+ 
+ 
+    def next(self):
+        if self.order:
+            return
+        if not self.position:
+            if self.dataclose[0] > self.sma[0]:
+                self.order = self.buy()
+ 
+        else:
+ 
+            if self.dataclose[0] < self.sma[0]:
+                # Keep track of the created order to avoid a 2nd order
+                self.order = self.sell()
+ 
+    def stop(self):
+        self.log('(MA Period %2d) Ending Value %.2f' %
+                 (self.params.maperiod, self.broker.getvalue()))
 
+
+class firstStrategy(bt.Strategy):
+    params = (
+        ('maxlevel', 70),
+        ('minlevel', 20),
+    )
+    
     def __init__(self):
         self.lastbuy = 0
         self.revenue = 0
@@ -27,16 +77,16 @@ class firstStrategy(bt.Strategy):
         print('%s, %s' % (dt.isoformat(), txt))
     def next(self):
         if not self.position:
-            if self.kd[-1] > 10 and self.kd[0] < 10:
+            if self.kd[-1] > self.params.minlevel and self.kd[0] < self.params.minlevel:
                 self.lastbuy = self.dataclose[0]
-                print('Buy, %.2f' % self.dataclose[0])
+#                print('Buy, %.2f' % self.dataclose[0])
                 self.buy(size=100)
         else:
-            if self.kd[-1] < 90 and self.kd[0] > 90:
+            if self.kd[-1] < self.params.maxlevel and self.kd[0] > self.params.maxlevel:
                 self.tradetime = self.tradetime + 1
                 self.revenue = self.dataclose[0] - self.lastbuy
                 self.totalrevenue = self.totalrevenue + self.revenue
-                print('Sell, %.2f' % self.dataclose[0])
+#                print('Sell, %.2f' % self.dataclose[0])
 #                print('Trade times ${}'.format(self.tradetime), 'Revenue ${}'.format(self.revenue), 'Total revenue ${}'.format(self.totalrevenue))
                 self.revenue = 0
                 self.lastbuy = 0
@@ -51,41 +101,44 @@ class firstStrategy(bt.Strategy):
 #                print('Sell, %.2f' % self.dataclose[0])
 #                self.sell(size=100) 
 
-    def notify_order(self, order):
-        if order.status in [order.Submitted, order.Accepted]:
-            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
-            return
-
-        # Check if an order has been completed
-        # Attention: broker could reject order if not enough cash
-        if order.status in [order.Completed]:
-            if order.isbuy():
-                self.log(
-                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-                    (order.executed.price,
-                     order.executed.value,
-                     order.executed.comm))
-
-                self.buyprice = order.executed.price
-                self.buycomm = order.executed.comm
-            else:  # Sell
-                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
-                         (order.executed.price,
-                          order.executed.value,
-                          order.executed.comm))
-
-            self.bar_executed = len(self)
-
-        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            self.log('Order Canceled/Margin/Rejected')
-
-        # Write down: no pending order
-        self.order = None
-    def notify_trade(self, trade):
-        if not trade.isclosed:
-            return
-        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
-                 (trade.pnl, trade.pnlcomm))
+#    def notify_order(self, order):
+#        if order.status in [order.Submitted, order.Accepted]:
+#            # Buy/Sell order submitted/accepted to/by broker - Nothing to do
+#            return
+#
+#        # Check if an order has been completed
+#        # Attention: broker could reject order if not enough cash
+#        if order.status in [order.Completed]:
+#            if order.isbuy():
+#                self.log(
+#                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+#                    (order.executed.price,
+#                     order.executed.value,
+#                     order.executed.comm))
+#
+#                self.buyprice = order.executed.price
+#                self.buycomm = order.executed.comm
+#            else:  # Sell
+#                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+#                         (order.executed.price,
+#                          order.executed.value,
+#                          order.executed.comm))
+#
+#            self.bar_executed = len(self)
+#
+#        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+#            self.log('Order Canceled/Margin/Rejected')
+#
+#        # Write down: no pending order
+#        self.order = None
+#    def notify_trade(self, trade):
+#        if not trade.isclosed:
+#            return
+#        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+#                 (trade.pnl, trade.pnlcomm))
+    def stop(self):
+        self.log('(minlevel %2d) (maxlevel %2d) Ending Value %.2f' %
+                 (self.params.minlevel, self.params.maxlevel, self.broker.getvalue()))
 def tw2bt():
     res_df = pd.read_pickle('res_df.pkl')
 #    print(res_df)
@@ -122,7 +175,18 @@ def main():
     cerebro = bt.Cerebro()
     
     #Add our strategy
-    cerebro.addstrategy(firstStrategy)
+#    cerebro.addstrategy(firstStrategy)
+    
+#    cerebro.optstrategy(
+#        MyStrategy,
+#        maperiod=range(10, 31))
+
+    cerebro.optstrategy(
+        firstStrategy,
+        minlevel=range(5, 40),
+        maxlevel=range(60, 100),
+        
+        )
     
     #Get Apple data from Yahoo Finance.
     #data = bt.feeds.Quandl(
@@ -150,7 +214,7 @@ def main():
         parse_dates=True,
         index_col=0,
     )
-    dataframe = dataframe.head(200)
+#    dataframe = dataframe.head(200)
     print(dataframe)
     
     # Pass it to the backtrader datafeed and add it to the cerebro
@@ -168,18 +232,17 @@ def main():
     # Set our desired cash start
     cerebro.broker.setcash(startcash)
     
-
-    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name = 'SharpeRatio')
-    cerebro.addanalyzer(bt.analyzers.DrawDown, _name='DW')
+#    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+#    cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name = 'SharpeRatio')
+#    cerebro.addanalyzer(bt.analyzers.DrawDown, _name='DW')
 
     # Run over everything
     results = cerebro.run()
     
-    start = results[0]
-    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
-    print('SR:', start.analyzers.SharpeRatio.get_analysis())
-    print('DW:', start.analyzers.DW.get_analysis())
+#    start = results[0]
+#    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+#    print('SR:', start.analyzers.SharpeRatio.get_analysis())
+#    print('DW:', start.analyzers.DW.get_analysis())
 
     #Get final portfolio Value
     portvalue = cerebro.broker.getvalue()
@@ -190,7 +253,7 @@ def main():
     print('P/L: ${}'.format(pnl))
     
     #Finally plot the end results
-    cerebro.plot(style='candlestick')
+#    cerebro.plot(style='candlestick')
 
 if __name__ == '__main__':
     main()
